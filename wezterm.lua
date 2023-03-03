fa64e802ca048378cb869dc0d9e93d7838f7d5ff
distek <distek@protonmail.com>
2023-03-03 06:43:27 -0800

    Cleanup

--- wezterm.lua
+++ wezterm.lua
@@ -65,7 +65,11 @@ return {
 
 	enable_wayland = true,
 
-	font = wezterm.font("FiraCode Nerd Font Mono", { weight = "Regular", stretch = "Normal", style = "Normal" }),
+	font = wezterm.font("FiraCode Nerd Font Mono", {
+		weight = "Regular",
+		stretch = "Normal",
+		style = "Normal",
+	}),
 
 	colors = require("colors"),
 	keys = require("keybindings"),
@@ -79,13 +83,6 @@ return {
 	},
 
 	window_background_opacity = 0.9,
-
-	-- default_prog = {
-	--     '/bin/zsh',
-	--     '-l',
-	--     '-c',
-	--     '/bin/tmux'
-	-- },
 
 	enable_tab_bar = true,
 	use_fancy_tab_bar = false,
